a387c1390deb164f0301c722d7ebea24e766d571
Tung <sky250331@gmail.com>
2021-08-30 15:39:31 +0800

    Update 2021-08-30T07:39:14.799Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,21 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.courseSwiper', {
+  // slidesPerColumnFill: 'horizontal',
+  slidesPerView: 2,
+  grid: {
+    fill: 'row',
+    rows: 2
+  },
+  spaceBetween: 30,
+  breakpoints: {
+    992: {
+      slidesPerView: 1.5,
+      loop: true,
+      grid: {
+        rows: 1
+      }
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
