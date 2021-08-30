7a43b9ff6e5ae80080c966f0cc2215178b463168
Tung <sky250331@gmail.com>
2021-08-30 15:37:54 +0800

    course commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,19 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.courseSwiper', {
+  // slidesPerColumnFill: 'horizontal',
+  slidesPerView: 2,
+  grid: {
+    fill: 'row',
+    rows: 2,
+  },
+  spaceBetween: 30,
+
+  breakpoints: {
+    992: {
+      slidesPerView: 1.5,
+      loop: true,
+      grid: {
+        rows: 1,
+      },
+    },
+  },
 });
