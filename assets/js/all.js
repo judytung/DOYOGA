5b94f0e8bdad76c63f25b20f4515d6bdc66454b2
Tung <sky250331@gmail.com>
2021-09-01 17:55:13 +0800

    Update 2021-09-01T09:55:11.076Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,7 @@
 "use strict";
 
+/*index course*/
 var swiper = new Swiper('.courseSwiper', {
-  // slidesPerColumnFill: 'horizontal',
   slidesPerView: 2,
   grid: {
     fill: 'row',
@@ -17,6 +17,8 @@ var swiper = new Swiper('.courseSwiper', {
     }
   }
 });
+/*index 聽他們怎麼說*/
+
 var CusSwiper = new Swiper('.cusSwiper', {
   slidesPerView: 1,
   grid: {
@@ -42,6 +44,52 @@ var CusSwiper = new Swiper('.cusSwiper', {
   navigation: {
     nextEl: ".swiper-btn-next",
     prevEl: ".swiper-btn-prev"
+  },
+  autoplay: {
+    delay: 3500
+  }
+});
+/*reservation collapse*/
+
+var firstBtn = document.querySelector('.firstPlanBtn');
+var shortPlan = document.querySelector('.short-plan');
+var longPlan = document.querySelector('.long-plan');
+var firstPlan = document.querySelector('.firstPlan-card');
+firstBtn.addEventListener('click', function () {
+  shortPlan.classList.add('d-none');
+  longPlan.classList.add('d-none');
+  firstPlan.classList.add('border-4');
+});
+var planLevelCard = document.querySelector('.planLevel-card');
+var checkCircle = document.querySelector('.check-circle');
+var choosePlan = document.querySelector('.choose-plan');
+var keepReser = document.querySelector('.keepReser');
+planLevelCard.addEventListener('click', function () {
+  planLevelCard.classList.add('border-white');
+  planLevelCard.classList.add('border-4');
+  checkCircle.classList.add('opacity-100');
+  choosePlan.classList.add('d-block');
+  keepReser.classList.add('d-block');
+});
+/*reservation swiper*/
+
+var Planswiper = new Swiper('.planSwiper', {
+  slidesPerView: 1,
+  grid: {
+    fill: 'row',
+    rows: 3
+  },
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.1,
+      grid: {
+        rows: 1
+      }
+    },
+    992: {
+      slidesPerView: 3
+    }
   }
 });
 //# sourceMappingURL=all.js.map
