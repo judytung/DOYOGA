c08e02e3a35e70e4cea9de849e7012185e9a27f1
Tung <sky250331@gmail.com>
2021-09-05 04:19:49 +0800

    Update 2021-09-04T20:19:46.405Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -125,6 +125,34 @@ var CourseTeacherswiper = new Swiper('.course-teacher-swiper', {
     }
   }
 });
+/*surrounding course-recommend-swiper*/
+
+var TeacherRecommendswiper = new Swiper('.course-recommend-swiper', {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  grid: {
+    fill: 'row',
+    rows: 1
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      grid: {
+        rows: 1
+      }
+    },
+    992: {
+      slidesPerView: 4.2,
+      grid: {
+        rows: 1
+      }
+    }
+  },
+  autoplay: {
+    delay: 2500
+  },
+  loop: true
+});
 /*datepicker*/
 
 var elem = document.querySelector('input[name="datepicker"]');
